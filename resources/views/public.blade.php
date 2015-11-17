b007a75d7358504082515104b4f355a48b412c4d
Trevor Sawler <trevor.sawler@gmail.com>
2015-11-17 10:58:22 -0400

    Fix typo

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>RecycleNB</title>
+    <title>Recycle NB</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
