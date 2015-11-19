4d9092d70db09c5de93fe1378768325c85520f71
Trevor Sawler <trevor.sawler@gmail.com>
2015-11-19 11:39:46 -0400

    save reg to db

--- app/Http/Controllers/TireController.php
+++ app/Http/Controllers/TireController.php
@@ -1,11 +1,41 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\TireRegistration;
+use App\TireRegistrationOfficer;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Input;
 
 class TireController extends Controller {
 
     public function postTire() {
-        return "Display confirmation screen here...";
+        $registration = new TireRegistration;
+        $registration->fill(Input::all());
+        $registration->ip_address = Request::getClientIp();
+        $registration->save();
+        $registration_id = $registration->id;
+
+        $officer_names = Input::get('officer_name');
+        $officer_titles = Input::get("officer_title");
+        $officer_addresses = Input::get("officer_address");
+        $officer_provinces = Input::get("officer_province");
+        $officer_zips = Input::get("officer_zip");
+
+
+        foreach ($officer_names as $id => $value){
+            if (($value != "") && ($value != null)) {
+                $officer = new TireRegistrationOfficer;
+                $officer->tire_registration_id = $registration_id;
+                $officer->officer_name = $officer_names[$id];
+                $officer->officer_title = $officer_titles[$id];
+                $officer->officer_address = $officer_addresses[$id];
+                $officer->officer_province = $officer_provinces[$id];
+                $officer->officer_zip = $officer_zips[$id];
+                $officer->save();
+            }
+        }
+
+        return "DOne tire";
     }
 }
 
--- app/TireRegistration.php
+++ app/TireRegistration.php
@@ -4,8 +4,37 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class TireRegistration extends Model
-{
+class TireRegistration extends Model {
+
+    protected $fillable = [
+        'name_of_applicant',
+        'sell_tires',
+        'remittance',
+        'type_of_company',
+        'head_physical_location',
+        'head_mailing_address',
+        'head_zip',
+        'nb_physical_location',
+        'nb_mailing_address',
+        'nb_province_state',
+        'nb_zip',
+        'contact_name',
+        'contact_physical_location',
+        'contact_mailing_address',
+        'contact_province_state',
+        'contact_zip',
+        'contact_phone',
+        'contact_email',
+        'agent_name',
+        'agent_company_name',
+        'agent_physical_location',
+        'agent_mailing_address',
+        'agent_province_state',
+        'agent_zip',
+        'agent_phone',
+        'agent_email',
+    ];
+
     public function officers()
     {
         return $this->hasMany('TireRegistrationOfficer');
