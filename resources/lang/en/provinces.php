35a20107c4e81277aa0e4415c25edf90f7d6aaf3
Trevor Sawler <trevor.sawler@gmail.com>
2015-12-02 14:38:08 -0400

    translation

---
+++ resources/lang/en/provinces.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'bc'  => 'British Columbia',
+    'pei' => 'Prince Edward Island',
+    'nwt' => 'Northwest Territories',
+    'nb'  => 'New Brunswick',
+    'ns'  => 'Nova Scotia',
+    'nl'  => 'Newfoundland and Labrador',
+    'ca' => 'California',
+    'nc' => 'North Carolina',
+    'sc' => 'South Carolina',
+    'nd' => 'North Dakota',
+    'sd' => 'South Dakota',
+    'fl' => 'Florida',
+    'ga' => 'Georgia',
+    'hi' => 'Hawaii',
+    'la' => 'Louisiana',
+    'nm' => 'New Mexico',
+    'pa' => 'Pennsylvania',
+    'va' => 'Virginia',
+    'wv' => 'West Virginia',
+
+];
---
+++ resources/lang/fr/provinces.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'bc'  => 'Colombie-Britannique',
+    'pei' => 'l\'île du Prince-Édouard',
+    'nwt' => 'Territoires du Nord-Ouest',
+    'nb'  => 'Nouveau-Brunswick',
+    'ns'  => 'Nouvelle-Écosse',
+    'nl'  => 'Terre-Neuve-et-Labrador',
+    'ca' => 'Californie',
+    'nc' => 'Caroline du Nord',
+    'sc' => 'Caroline du Sud',
+    'nd' => 'Dakota du Nord',
+    'sd' => 'Dakota du Sud',
+    'fl' => 'Floride',
+    'ga' => 'Géorgie',
+    'hi' => 'Hawaï',
+    'la' => 'Louisiane',
+    'nm' => 'Nouveau-Mexique',
+    'pa' => 'Pennsylvanie',
+    'va' => 'Virginie',
+    'wv' => 'Virginie-Occidentale',
+
+];
